fa51d12f44cd1be48f85c6b24e3a22ed9b87da6f
DevMilk <ugurkkkkkk@hotmail.com>
2020-09-23 15:02:18 +0300

    Added Indicator Functions

---
+++ src/test/java/UnitTests.java
@@ -0,0 +1,26 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ugur
+ */
+public class UnitTests {
+    
+    @Test
+    public void test(){
+        System.out.println("aa");
+        assertEquals(1,1);
+    }
+    @Test
+    public void test2(){
+        System.out.println("aa");
+        assertEquals(1,1);
+    }
+}
